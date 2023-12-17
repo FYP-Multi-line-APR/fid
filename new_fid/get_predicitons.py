3b963157e4876dc293e71dbad6290ef49354510e
Ayesh Vininda <ayeshgk@gmail.com>
2023-12-17 18:27:23 +0530

    bug fix

--- new_fid/get_predicitons.py
+++ new_fid/get_predicitons.py
@@ -43,6 +43,21 @@ if __name__ == "__main__":
     opt = options.parse()
     src.slurm.init_distributed_mode(opt)
     src.slurm.init_signal_handler()
+
+    dir_path = Path(opt.checkpoint_dir)/opt.name
+    directory_exists = dir_path.exists()
+
+    if opt.is_distributed:
+        torch.distributed.barrier()
+    dir_path.mkdir(parents=True, exist_ok=True)
+    if opt.write_results:
+        (dir_path / 'prediction_results').mkdir(parents=True, exist_ok=True)
+    logger = src.util.init_logger(opt.is_main, opt.is_distributed, Path(opt.checkpoint_dir) / opt.name / 'run.log')
+    if not directory_exists and opt.is_main:
+        options.print_options(opt)
+
+
+
     opt.train_batch_size = opt.per_gpu_batch_size * max(1, opt.world_size)
 
     tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_NAME, return_dict=False)
@@ -71,7 +86,7 @@ if __name__ == "__main__":
     model = model_class.from_pretrained(opt.model_path)
     model = model.to(opt.device)
 
-    logger = src.util.init_logger(opt.is_main, opt.is_distributed, Path(opt.checkpoint_dir) / opt.name / 'run.log')
+    # logger = src.util.init_logger(opt.is_main, opt.is_distributed, Path(opt.checkpoint_dir) / opt.name / 'run.log')
     logger.info("Start getting predictions")
 
     predictions = get_predictions(model, eval_dataset, eval_dataloader, tokenizer, opt)
